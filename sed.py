20b2559e638093095b3d1de30793e26d05126b43
Serat Mahmud Saad <98350695+seratsaad@users.noreply.github.com>
2024-05-01 08:07:16 -0500

    Updated file

--- sed.py
+++ sed.py
@@ -1,30 +1,31 @@
 from SEDFit.sed import SEDFit
 import matplotlib.pyplot as plt
 from astropy.io import fits
+import numpy as np
+from tqdm import tqdm
+from astropy.table import Table
+
+
 
 hdul = fits.open('ctts_veiling.fits')
 data = hdul[1].data
-ra = data['RACAT'][0]
-dec = data['DECCAT'][0]
-teff = data['pred_logteff_1'][0]
-logg = data['pred_logg_1'][0]
 
-print(ra,  dec, teff, logg)
+print(len(data))
+
+for i in tqdm(range(len(data))):
+    try:
+        ra = data['RACAT'][i]
+        dec = data['DECCAT'][i]
+        teff = data['pred_logteff_1'][i]
+        logg = data['pred_logg_1'][i]
+
+        x=SEDFit(ra, dec, 1, download_gaia=True)
+        x.addguesses(teff=10**teff, logg=logg,feh=0)
+        x.sed['model']=x.mags
+        table = x.sed
+        print(table)
+        table.write(f'fits_files/{ra}_{dec}.fits', format='fits', overwrite=True)
+    except:
+        continue
 
 
-x=SEDFit(ra, dec, 1)
-x.addguesses()
-x.addrange()
-x.fit()
-print("Distance: {} pc".format(x.getdist()))
-print("AV: {} mag".format(x.getav()))
-print("Radius: {} Rsun".format(x.getr()))
-print("Teff: {} K".format(x.getteff()))
-print("Log g: {} ".format(x.getlogg()))
-print("Fe/H: {}".format(x.getfeh()))
-print("Chi squared: {}".format(x.getchisq()))
-ax = x.makeplot()
-plt.show()
-plt.savefig('fit2.png')
-x.sed['model']=x.mags
-x.sed
